96e1d760382c36aab061cdb500dec0c1fde67cfb
Jaco-Miras <miras.jaco@gmail.com>
2022-07-17 19:01:58 +0800

    commit

--- src/pages/Details/Details.jsx
+++ src/pages/Details/Details.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useAppContext } from "../../context/AppContext";
 import { useParams } from "react-router-dom";
-import Stats from "../../component/Stats";
 
 function Details() {
   const { getPokemonDetails } = useAppContext();
